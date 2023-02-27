add5185ffb388b6a00a397e8558e3894ef1f659c
Laurine RAT <99804745+Laurine2905@users.noreply.github.com>
2023-02-27 15:51:18 +0100

    ajout objet dans categorie

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- Initialisation des tables
-INSERT INTO Country(code, name) VALUES
-    ('FR', 'France'), -- Les clés sont auto-générées
-    ('UK', 'United Kingdom'),
-    ('US', 'United States of America');
+INSERT INTO Categorie(Categorie_nom, Categorie_description, Categorie_photo) VALUES
+    ('Appareils d anesthésie', NULL,'https://photos.google.com/photo/AF1QipNpiGCTV_NsLZr6LX6TBP0xmsoKYVVcr-UM3Au8'), -- Les clés sont auto-générées
+    ('Ventilateur d anesthésie', NULL,'https://photos.google.com/photo/AF1QipOi2cm-3IxlCJ4DjdonRC2jGf95-FwJl16kyZBk'),
+    ('Ventilateur de réanimation', 'je suis une description','https://photos.google.com/photo/AF1QipOaUupqUsDpyTBMaZMIJ6TZBrnlj-QqV2hyaQ40');
