413d2012a50acf5df8c333a0b0e9ee96fca4f662
Laurine RAT <99804745+Laurine2905@users.noreply.github.com>
2023-02-27 09:30:18 +0100

    repository en construction categorie ok

---
+++ backend/src/main/java/isis/projet/backend/dao/CategorieRepository.java
@@ -0,0 +1,15 @@
+package isis.projet.backend.dao;
+
+import jakarta.persistence.Tuple;
+import isis.projet.backend.entity.Categorie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
+
+
+public interface CategorieRepository extends JpaRepository<Categorie, Integer> {
+
+// Liste des objets dans une catégorie
+    public List<Tuple> allObjetInCategorie();
+}
